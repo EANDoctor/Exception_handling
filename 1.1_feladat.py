7aac0d28ba4f9d925db8befa22f754c1fcce33c3
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2025-11-19 09:12:58 +0100

    Update 1.1_feladat.py

--- 1.1_feladat.py
+++ 1.1_feladat.py
@@ -2,10 +2,11 @@
 1. Feladat
 Írj egy programot, ami a felhasználótól három egész számot számot kér be egyesével, ezeket eltárolja egy listában, majd a képernyőre kiírja a lista tartalmát! Ha a felhasználó nem számot ad meg, kapjon hibaüzenetet, és ismétlődjön meg a bekérés! 
 """
+x = 3
 numbers = []
-while len(numbers) < 3:
+while len(numbers) < x:
     try:
-        num = int(input("Kérek 3 egész számot: "))
+        num = int(input(f"Kérek {x} egész számot: "))
         numbers.append(num)
     except ValueError as e:
         print(f"{e} \nNem számot adtál meg!")
